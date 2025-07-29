149019ac36d5491725f7fc5531a1eca6f13a80d9
Manny <goodcupcakes344@gmail.com>
2025-07-29 15:12:05 -0400

    Changed templates from database

--- newapp/views.py
+++ newapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Question
+from django.utils import timezone 
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    questions = Question.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request, "newapp/home.html",{'questions': questions})
