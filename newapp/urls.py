c8056106571c91a7308231ce7a31b34e4f0f96a8
Manny <goodcupcakes344@gmail.com>
2025-07-29 22:53:04 -0400

    added create/edit with new views

--- newapp/urls.py
+++ newapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name = "home"),
-    path("post/<int:pk>/",views.question_detail, name = "question_detail")
+    path("question/<int:pk>/",views.question_detail, name = "question_detail"),
+    path("question/new/", views.question_new, name = "question_new"),
+    path("question/<int:pk>/edit/", views.question_edit,name = "question_edit")
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from .models import Question
-from django.utils import timezone 
+from django.utils import timezone
+from .forms import QuestionForm
 
 
 # Create your views here.
@@ -12,3 +13,30 @@ def home(request):
 def question_detail(request,pk):
     question = get_object_or_404(Question,pk=pk)
     return render(request, "newapp/question_detail.html",{"questions":question})
+
+def question_new(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.published_date = timezone.now()
+            question.save()
+            return redirect("question_detail",pk=question.pk)
+    else:
+        form = QuestionForm()
+    return render(request, "newapp/question_edit.html",{"form":form} )
+
+def question_edit(request,pk):
+    question = get_object_or_404(Question,pk=pk)
+    if request.method == "POST":
+        form = QuestionForm(request.POST,instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.published_date = timezone.now()
+            question.save()
+            return redirect("question_detail",pk=question.pk)
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, "newapp/question_edit.html",{"form":form} )
+    
+    
