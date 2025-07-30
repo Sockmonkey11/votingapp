63b82af7ccaa963379a8e66e8f1e54d74e55ea7b
Manny <goodcupcakes344@gmail.com>
2025-07-29 20:23:56 -0400

    Added new view and template for question_detail

--- newapp/urls.py
+++ newapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home, name = "home")
+    path("",views.home, name = "home"),
+    path("post/<int:pk>/",views.question_detail, name = "question_detail")
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Question
 from django.utils import timezone 
 
@@ -8,3 +8,7 @@ from django.utils import timezone
 def home(request):
     questions = Question.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, "newapp/home.html",{'questions': questions})
+
+def question_detail(request,pk):
+    question = get_object_or_404(Question,pk=pk)
+    return render(request, "newapp/question_detail.html",{"questions":question})
